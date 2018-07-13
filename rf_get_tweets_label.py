9b327b908024c7a8d2ba3095c48ffe89febcce94
kotaaaa <kota.k.1132.pda@gmail.com>
2018-07-13 11:10:01 +0900

    second commit in twitter

--- rf_get_tweets_label.py
+++ rf_get_tweets_label.py
@@ -8,7 +8,7 @@ import re
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 
-'''データベースの設定ファイルを読み込む'''
+'''データベースの設定ファイルを読み込むaaaa'''
 def get_db_name():
     local_db = {}
     with open('table_tweet.conf','r') as f:
